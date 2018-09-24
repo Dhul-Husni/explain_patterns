3ef4abadf643db4a4286d87d2282bfb956f44a28
Dhul-husni <sir3n.sn@gmail.com>
2018-09-24 19:09:06 +0300

    scaffold needed modules

--- lib/explain_pattern.rb
+++ lib/explain_pattern.rb
@@ -1,5 +1,18 @@
 require "explain_pattern/version"
+require "regexp_pattern"
+require "terminal-table/import"
 
 module ExplainPattern
-  # Your code goes here...
+
+  regex = /a?(b+(c)d)*(?<name>[0-9]+)(?=abc)/
+
+  module Tree
+    tree = Regexp::Parser.parse(regex)
+  end
+
+  module Table
+  end
+
+  class String
+  end
 end
