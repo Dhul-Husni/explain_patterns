317221ed8af450b8d98dc430243c51811962b68b
Dhul-husni <sir3n.sn@gmail.com>
2018-09-27 14:08:40 +0300

    cleanup

--- lib/explain_pattern.rb
+++ lib/explain_pattern.rb
@@ -5,17 +5,11 @@ require 'tree_support'
 module ExplainPattern
   class Tree
 
-    # Create a new regexp tree
-    #
-    # @param [String] regular expression
     def initialize(regexp)
       @regexp = Regexp::Parser.parse regexp
       display_tree
     end
 
-    # Kick off the tree construction
-    #
-    # @param [String] regular expression
     def struct
       tree = Node.new(@regexp) { traverse_exp @regexp }
       Node.clear_temp
@@ -40,33 +34,14 @@ module ExplainPattern
         end
       end
 
-      # Create a new node/leaf on the tree
-      #
-      # @param [Regexp::Expression]
-      # @param [Block] How the tree should be created
-      # @example
-      #   root = TreeSupport::Node.new("ROOT") do
-      #       add "A" do
-      #         add "B" do
-      #           add "C"
-      #         end
-      #       end
-      #     end
-      #     puts TreeSupport.tree(root)
-      #     > ROOT
-      #     >  A
-      #     >    B
-      #     >      C
       def initialize(regexp, &block)
         @regexp   = regexp
-        @name     = @regexp.to_s # Root of the tree
+        @name     = @regexp.to_s
         @children = []
         @temp     = Node.temp
         instance_eval(&block) if block_given?
       end
 
-      # Actively add a new node to the tree
-      #
       def add(*args, &block)
         tap do
           children << self.class.new(*args, &block).tap do |instance|
@@ -75,9 +50,6 @@ module ExplainPattern
         end
       end
 
-      # Traverses each exp; creating new Nodes
-      #
-      # @param [Regexp::Expression]
       def traverse_exp(regexp)
         regexp.traverse do |event, exp|
           unless @temp.include? exp
